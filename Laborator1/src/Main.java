4afcfdf8db51a557c710348dc716660c17801f2c
Adrian <adrianoinacaru5@gmail.com>
2023-10-06 20:21:58 +0300

    implemented methods: roundedGrades + maxRoundedGrade

--- Laborator1/src/Main.java
+++ Laborator1/src/Main.java
@@ -3,9 +3,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Ubung1 notenProzessor = new Ubung1();
-        int[] noten = {45, 23, 78, 69, 80, 19, 89, 100, 39, 13};
+        int[] noten = {45, 23, 78, 69, 80, 19, 89, 100, 39, 13, 48};
         //Ubung 1.1
         System.out.println("Nicht ausreichende Noten: " + Arrays.toString(notenProzessor.nichtAusreichend(noten)));
         System.out.println("Average grade = " + notenProzessor.averageGrade(noten));
+        System.out.println("All rounded grades = " + Arrays.toString(notenProzessor.roundedGrades(noten)));
+        System.out.println("Maximum rounded grade = " + notenProzessor.maxRoundedGrade(noten));
     }
 }
--- Laborator1/src/Ubung1.java
+++ Laborator1/src/Ubung1.java
@@ -1,9 +1,10 @@
 public class Ubung1 {
     //helper method to round the grades
     public int round(int note){
+        if (note < 38) return note;
         int nextFiveMultiple = note;
         while (nextFiveMultiple % 5 != 0) nextFiveMultiple++;
-        note = nextFiveMultiple;
+        if (nextFiveMultiple - note < 3) note = nextFiveMultiple;
         return note;
     }
 
@@ -32,7 +33,26 @@ public class Ubung1 {
         return (double)sum/noten.length;
     }
 
+    public int[] roundedGrades(int[] noten){
+        int[] roundedGrades = new int[noten.length];
+        for(int i = 0; i < noten.length; i++){
+            roundedGrades[i] = round(noten[i]);
+        }
+        return roundedGrades;
+    }
 
+    //here we calculate what the biggest possible difference between a multiple of five and the grade itself is
+    public int maxRoundedGrade(int[] noten){
+        int maxRounded = 0;
+        int maxRoundedGrade = 0;
+        for (var grade : noten){
+            if (round(grade) - grade > maxRounded){
+                maxRounded = round(grade) - grade;
+                maxRoundedGrade = grade;
+            }
+        }
+        return maxRoundedGrade;
+    }
 
 
 }
