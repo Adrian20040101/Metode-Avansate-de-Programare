308b279e9109bdd5ba027773d552c013c31113a7
Adrian <adrianoinacaru5@gmail.com>
2023-10-06 20:01:49 +0300

    implemented methods: nicht ausreichende Noten average grade

--- Laborator1/src/Main.java
+++ Laborator1/src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] noten = {45, 23, 78, 69, 80, 19, 89, 100};
+        Ubung1 notenProzessor = new Ubung1();
+        int[] noten = {45, 23, 78, 69, 80, 19, 89, 100, 39, 13};
         //Ubung 1.1
-        System.out.println();
+        System.out.println("Nicht ausreichende Noten: " + Arrays.toString(notenProzessor.nichtAusreichend(noten)));
+        System.out.println("Average grade = " + notenProzessor.averageGrade(noten));
     }
 }
--- Laborator1/src/Ubung1.java
+++ Laborator1/src/Ubung1.java
@@ -1,22 +1,38 @@
 public class Ubung1 {
     //helper method to round the grades
-    public static int round(int note){
+    public int round(int note){
         int nextFiveMultiple = note;
         while (nextFiveMultiple % 5 != 0) nextFiveMultiple++;
+        note = nextFiveMultiple;
         return note;
     }
 
-    public static int[] nichtAusreichend(int[] noten){
-        int[] intermediaryArray = new int[noten.length];
+    public int[] nichtAusreichend(int[] noten){
+        int gradesBelow40 = 0;
+        for (var grade : noten){
+            if (round(grade) < 40) gradesBelow40++;
+        }
+
+        int[] intermediaryArray = new int[gradesBelow40];
         int intermediaryIndex = 0;
         for (var note : noten){
-            if (round(note) <= 40) {
+            if (round(note) < 40) {
                 intermediaryArray[intermediaryIndex] = note;
                 intermediaryIndex++;
             }
         }
         return intermediaryArray;
     }
+
+    public double averageGrade(int[] noten){
+        int sum = 0;
+        for (var grade : noten){
+            sum += grade;
+        }
+        return (double)sum/noten.length;
+    }
+
+
 
 
 }
