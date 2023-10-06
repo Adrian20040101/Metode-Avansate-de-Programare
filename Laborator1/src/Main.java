19002294572cd3dc332fce8390b447f643f48b00
Adrian <adrianoinacaru5@gmail.com>
2023-10-06 20:43:01 +0300

    done exercise 2

--- Laborator1/src/Main.java
+++ Laborator1/src/Main.java
@@ -4,10 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Ubung1 notenProzessor = new Ubung1();
         int[] noten = {45, 23, 78, 69, 80, 19, 89, 100, 39, 13, 48};
-        //Ubung 1.1
+        //Ubung 1
         System.out.println("Nicht ausreichende Noten: " + Arrays.toString(notenProzessor.nichtAusreichend(noten)));
         System.out.println("Average grade = " + notenProzessor.averageGrade(noten));
         System.out.println("All rounded grades = " + Arrays.toString(notenProzessor.roundedGrades(noten)));
         System.out.println("Maximum rounded grade = " + notenProzessor.maxRoundedGrade(noten));
+        System.out.println("-".repeat(30));
+
+        //Ubung 2
+        Ubung2 ubung2 = new Ubung2();
+        int[] arrayOfNumbers = {12, 34, 21, 46, 864, 313, 54, 32, 11, 6, 85};
+        System.out.println("Maximum number in the array = " + ubung2.maximum(arrayOfNumbers));
+        System.out.println("Minimum number in the array = " + ubung2.minimum(arrayOfNumbers));
+        System.out.println("Maximum n-1 numbers sum in the array = " + ubung2.maxSum(arrayOfNumbers));
+        System.out.println("Minimum n-1 numbers sum in the array = " + ubung2.minSum(arrayOfNumbers));
     }
 }
---
+++ Laborator1/src/Ubung2.java
@@ -0,0 +1,39 @@
+public class Ubung2 {
+    public int maximum(int[] numbers){
+        int maximum = Integer.MIN_VALUE;
+        for (var number : numbers){
+            if (number > maximum) maximum = number;
+        }
+        return maximum;
+    }
+
+    public int minimum(int[] numbers){
+        int minimum = Integer.MAX_VALUE;
+        for (var number : numbers){
+            if (number < minimum) minimum = number;
+        }
+        return minimum;
+    }
+
+    //max sum of n - 1 numbers from the array
+    public int maxSum(int[] numbers){
+        int maxSum = 0;
+        for (var number : numbers){
+            if (number != minimum(numbers)){
+                maxSum += number;
+            }
+        }
+        return maxSum;
+    }
+
+    //min sum of n - 1 numbers from the array
+    public int minSum(int[] numbers){
+        int minSum = 0;
+        for (var number : numbers){
+            if (number != maximum(numbers)){
+                minSum += number;
+            }
+        }
+        return minSum;
+    }
+}
